705244d06c436963cd115eecbe4319bda7cb890d
ferideyildiz <74494589+ferideyildiz@users.noreply.github.com>
2020-11-17 22:14:24 +0300

    Update BirinciGun.java

--- src/BirinciGun.java
+++ src/BirinciGun.java
@@ -2,7 +2,8 @@
 public class BirinciGun {
 
 	public static void main(String[] args) {
-		System.out.println("11:57 - Birinci Gün");
-		System.out.println("Ýkinci Commit");
+		System.out.println("11:57 - Birinci GÃ¼n");
+		System.out.println("Ä°kinci Commit");
+		System.out.println("10:11 - Ikinci Gun" );
 	}
 }
